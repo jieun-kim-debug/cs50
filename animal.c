9111a657904fa61a7ddcdb93786ef151a44c8a19
jieun-kim-debug <ddokind@hmail.com>
2020-09-12 02:24:41 +0000

    clang

---
+++ animal.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    string animal = "what's your favorite animal?\n";
+    printf("My favorite animal is %s", animal);
+}
+
+//clang -o animal animal.c == make animal
+//clang is compile source code -> machine code
